bb2b204529ec59c7b4db86122c966ec315112f01
maarten.bassier <maarten.bassier@kuleuven.be>
2024-03-19 10:20:40 +0100

    structure repo

---
+++ utils/__init__.py
@@ -0,0 +1,23 @@
+"""
+Utilities 
+
+"""
+import numpy as np
+import time
+
+def time_funtion(func, *args):
+    """Measures how long the functions takes to run and returns the result 
+
+    Args:
+        func (function): The funtion to measure, write without ()
+        *args (Tuple) : The arguments for the funtion, pass as a tuple with a * in front to pass the arguments seperatly
+
+    Returns:
+        object: The result of the function
+    """
+
+    start = time.time()
+    result = func(*args)
+    end = time.time()
+    print("Completed function `" + func.__name__ + "()` in", np.round(end - start,3), "seconds")
+    return result 
