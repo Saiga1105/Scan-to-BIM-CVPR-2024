68b6129db1bf6151a56c2f3c4524fa91e4692c32
mbassier <maarten.bassier@kuleuven.be>
2024-04-09 09:24:24 +0200

    update submodule

--- scripts/test.py
+++ scripts/test.py
@@ -2,25 +2,13 @@
 import os
 import sys
 import context 
-# import sys
-# sys.path.append('../thirdparty/pointcept')
-# sys.path.append('../')
+
+print('This is how you import modules and submodules')
 
 import utils.t1_utils as t1
-# import pointcept as ptv3 # thirdparty.pointcept 
-# from thirdparty.pointcept import pointcept
-
-# from thirdparty.pointcept.pointcept.engines.defaults import create_ddp_model
-
-# export PYTHONPATH=$PYTHONPATH:'../thirdparty/pointcept'
-
-
-# from ptv3.engines.defaults import create_ddp_model
-
 from pointcept.engines.defaults import create_ddp_model  # Example function
 
-# from pointcept.engines.defaults import create_
 
 
-print('test')
+
 
