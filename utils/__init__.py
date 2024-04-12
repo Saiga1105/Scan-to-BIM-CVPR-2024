846d90639009287b1fd3902923d62484991a63eb
mbassier <maarten.bassier@kuleuven.be>
2024-04-12 15:59:50 +0200

    updated t1 scripts to CVPR data

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,20 +4,42 @@ Utilities
 """
 import numpy as np
 import time
+from pathlib import Path
 
-def time_funtion(func, *args):
-    """Measures how long the functions takes to run and returns the result 
+def timer(func):
+    """
+    Decorator that measures and prints the execution time of a function.
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # Capture the start time
+        result = func(*args, **kwargs)  # Call the function with its arguments
+        end_time = time.time()  # Capture the end time
+        print(f"Function {func.__name__} took {end_time - start_time:.4f} seconds to execute.")
+        return result  # Return the result of the function
+    return wrapper
+
+def get_list_of_files(folder: Path | str , ext: str = None) -> list:
+    """
+    Get a list of all filepaths in the folder and subfolders that match the given file extension.
 
     Args:
-        func (function): The funtion to measure, write without ()
-        *args (Tuple) : The arguments for the funtion, pass as a tuple with a * in front to pass the arguments seperatly
+        folder: The path to the folder as a string or Path object
+        ext: Optional. The file extension to filter by, e.g., ".txt". If None, all files are returned.
 
     Returns:
-        object: The result of the function
+        A list of filepaths that match the given file extension.
     """
-
-    start = time.time()
-    result = func(*args)
-    end = time.time()
-    print("Completed function `" + func.__name__ + "()` in", np.round(end - start,3), "seconds")
-    return result 
+    folder = Path(folder)  # Ensure the folder is a Path object
+    allFiles = []
+    # Iterate over all the entries in the directory
+    for entry in folder.iterdir():
+        # Create full path
+        fullPath = entry
+        # If entry is a directory then get the list of files in this directory 
+        if fullPath.is_dir():
+            allFiles += get_list_of_files(fullPath, ext=ext)
+        else:
+            # Check if file matches the extension
+            if ext is None or fullPath.suffix.lower() == ext.lower():
+                allFiles.append(fullPath.as_posix())
+    return allFiles
--- utils/t1_utils.py
+++ utils/t1_utils.py
@@ -1,19 +1,47 @@
 
 import time
+import os
+import laspy
+import geomapi
+import geomapi.utils.geometryutils as gmu
+import geomapi.utils as ut
+import numpy as np
+import torch
 
-def t1_time_funtion(func, *args):
-    """Measures how long the functions takes to run and returns the result 
+from utils import timer
 
-    Args:
-        func (function): The funtion to measure, write without ()
-        *args (Tuple) : The arguments for the funtion, pass as a tuple with a * in front to pass the arguments seperatly
 
-    Returns:
-        object: The result of the function
-    """
+@timer
+def handle_process(file_name, output_folder,cfg) -> None:
+    print(f'processing {file_name} ...')
 
-    start = time.time()
-    result = func(*args)
-    end = time.time()
-    print("Completed function `" + func.__name__ + "()` in", np.round(end - start,3), "seconds")
-    return result 
+    coords = []
+    scene_id = os.path.basename(file_name)
+    name, _ = os.path.splitext(scene_id)    
+
+    # Read LAS/LAZ
+    las = laspy.read(file_name)
+    
+    #conver to pcd if no normals are present
+    pcd = gmu.las_to_pcd(las)
+    pcd.estimate_normals()
+    pcd.orient_normals_to_align_with_direction()
+    
+    coords = np.stack([las.x, las.y, las.z], axis=1)
+    colors = np.stack([las.red, las.green, las.blue], axis=1).astype(np.uint8)
+    normals = np.asarray(pcd.normals)
+    # verticality = np.nan_to_num(las.verticality)
+    # max = np.max(verticality)
+    # verticality = verticality / (max / 2.) - 1.
+    
+    # Remap las['classes'] to 0,1,2,3,4,5 based on values in cfg['data']['labels']
+    class_mapping = {label: i for i, label in enumerate(cfg['data']['labels'])}
+    remapped_classes = np.array([class_mapping[label] for label in las['classes']])
+    save_dict = dict(coord=coords, color=colors, normal=normals, scene_id=scene_id, semantic_gt=remapped_classes)
+    
+    
+    # save_dict = dict(coord=coords, color=colors, normal=normals, scene_id=scene_id, semantic_gt=las['classes'].astype(int))    
+
+    torch.save(save_dict, os.path.join(output_folder, f"{name}.pth"))
+    
+
